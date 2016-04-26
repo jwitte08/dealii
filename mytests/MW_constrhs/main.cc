c0f014c1d559e1efe9772a42ddafeffc47a543b1
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2016-04-26 10:21:25 +0200

    dealii no changes. first bm_report.

--- mytests/MW_constrhs/main.cc
+++ mytests/MW_constrhs/main.cc
@@ -54,27 +54,44 @@ void RHSIntegrator<dim>::face(dealii::MeshWorker::DoFInfo<dim> &,
                               typename dealii::MeshWorker::IntegrationInfo<dim> &) const
 {}
 
+const unsigned int dim = 2;
+const unsigned int degree = 1;
+
+dealii::Triangulation<2> tr;
+const dealii::FE_DGQ<dim> fe{degree};
+dealii::DoFHandler<dim> dof_handler{tr};
+const dealii::MappingQ1<dim> mapping;
+
+bool tr_refined = false;
+int prev_range_x = 0;
+
 // MAIN
-void test01 (benchmark::State &state)
+void mw_constrhs (benchmark::State &state)
 {
+  dealii::MultithreadInfo::set_thread_limit (state.range_y());
+  
+  // refine of initial tria
+  if(!tr_refined)
+    {
+      dealii::GridGenerator::hyper_cube (tr,0.,1.);
+      tr.refine_global(state.range_x());
+      dof_handler.distribute_dofs(fe);
+      tr_refined = true;
+      prev_range_x = state.range_x();
+      std::cout << "initial refine" << std::endl;
+    }
+  // re-refine for new tria
+  if(prev_range_x != state.range_x() && tr_refined)
+    {
+      tr.refine_global(state.range_x()-prev_range_x);
+      dof_handler.distribute_dofs(fe);
+      prev_range_x = state.range_x();
+      std::cout << "refine again" << std::endl;
+    }
+      
   while(state.KeepRunning())
     {
-      dealii::MultithreadInfo::set_thread_limit (state.range_y());
-      const unsigned int dim = 2;
-      const unsigned int degree = 1;
-
-      dealii::Triangulation<dim> tria;
-      const dealii::FE_DGQ<dim> fe{degree};
-      dealii::DoFHandler<dim> dof_handler{tria};
-      const dealii::MappingQ1<dim> mapping;
       dealii::Vector<double> rhs;
-
-      dealii::GridGenerator::hyper_cube (tria,0.,1.);
-      tria.refine_global(state.range_x());
-
-      dof_handler.distribute_dofs(fe);
-      //dof_handler.initialize_local_block_info();
-
       rhs.reinit(dof_handler.n_dofs());
 
       dealii::MeshWorker::IntegrationInfoBox<dim> info_box;
@@ -85,28 +102,44 @@ void test01 (benchmark::State &state)
 	dealii::update_values | dealii::update_gradients;
       info_box.add_update_flags(update_flags, true, true, true, true);
       info_box.initialize(fe, mapping);
-
+      
       dealii::MeshWorker::DoFInfo<dim> dof_info(dof_handler);
-
+      
       dealii::MeshWorker::Assembler::ResidualSimple<dealii::Vector<double> > rhs_assembler;
       dealii::AnyData data;
       data.add<dealii::Vector<double>* >(&rhs, "RHS");
       rhs_assembler.initialize(data);
-
-      RHSIntegrator<dim> rhs_integrator;
       
+      RHSIntegrator<dim> rhs_integrator;
       dealii::MeshWorker::integration_loop<dim, dim>(dof_handler.begin_active(),
 						     dof_handler.end(),
 						     dof_info, info_box,
 						     rhs_integrator, rhs_assembler);
-      //rhs.print(std::cout);
     }
+  //rhs.print(std::cout);
 }
 
-BENCHMARK(test01)
+static void CustomArguments(benchmark::internal::Benchmark* b) {
+  for (int x = 6; x <= 12 ; x+=2)
+    {
+      if(x <= 10)
+	{
+	  int y0 = (x <= 8) ? 1 : 4;
+	  for (int y = y0; y <= 4; y++)
+	    b->ArgPair(x, y);
+	  for (int y = 6; y <= x; y+=2)
+	    b->ArgPair(x, y);
+	}
+      else
+	for (int y = 12; y <= 24; y+=4)
+	  b->ArgPair(x, y);
+    }
+  b->ArgPair(10, 12);
+}
+
+BENCHMARK(mw_constrhs)
 ->Threads(1)
-->ArgPair(6,1)->ArgPair(6,8)->ArgPair(6,16)
-->ArgPair(8,1)->ArgPair(8,8)->ArgPair(8,16)
-->ArgPair(10,1)->ArgPair(10,8)->ArgPair(10,16)
+->Apply(CustomArguments)
 ->UseRealTime();
+
 BENCHMARK_MAIN()
